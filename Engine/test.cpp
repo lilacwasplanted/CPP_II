ae115185f1ee46f6a44d95e6a4512a00a39523e2
lilacwasplanted <lhollo5823@gmail.com>
2024-06-28 09:53:39 -0600

    Add project files.

---
+++ Engine/test.cpp
@@ -0,0 +1,10 @@
+
+#include "test.h"
+#include <iostream>
+
+
+using namespace std;
+
+void print() {
+	cout << "Yooo" << endl;
+}
---
+++ Game/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SDL.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cerr << "Error initializing SDL : "
+	}
+}
